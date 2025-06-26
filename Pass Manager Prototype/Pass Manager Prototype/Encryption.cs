6173c6d3334575e1f28fd641e73e49293729c8f8
Dakota Brown <bdakota321@gmail.com>
2025-06-25 20:02:17 -0400

    Full Build
    The full build of this console application.

---
+++ Pass Manager Prototype/Pass Manager Prototype/Encryption.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Org.BouncyCastle.Crypto;
+
+namespace Pass_Manager
+{
+    public class Encryption
+    {
+        public static byte[] GenerateSalt()
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            return salt;
+        }
+        public static string GenerateRandomPassword()
+        {
+            string characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm`!@#$%^&*()_-+={}[]|;:',<>./?1234567890";
+            int passwordLength = 15;
+            string password = "";
+
+            for(int i = 0; i < passwordLength; i++)
+            {
+                int index = RandomNumberGenerator.GetInt32(0, characters.Length);
+                password += characters[index];
+            }
+
+            return password;
+        }
+        public static byte[] GenerateIv()
+        {
+            using (Aes myAes = Aes.Create())
+            {
+                return myAes.IV;
+            }
+        }
+        public static byte[] GenerateKey(string email, string password, byte[] salt)
+        {
+            // A secure string to be hashed
+            string keyString = email + password;
+            // Hash the string to create the key
+            return Rfc2898DeriveBytes.Pbkdf2(keyString, salt, 500000, new HashAlgorithmName("SHA256"), 32);
+        }
+        public static string EncryptPassword_PBKDF2(string password, byte[] salt)
+        {
+            byte[] encryptedPassword = Rfc2898DeriveBytes.Pbkdf2(password, salt, 250000, new HashAlgorithmName("SHA512"), 64);
+            return Convert.ToBase64String(encryptedPassword);
+        }
+        public static string EncryptPassword_AES(string password, byte[] key, string Iv)
+        {
+            byte[] encrypted;
+            using (Aes myAes = Aes.Create())
+            {
+                encrypted = EncryptStringToBytes_Aes(password, key, Convert.FromBase64String(Iv));
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+        public static string DecryptPassword_AES(string encryption, byte[] key, string Iv)
+        {
+            using (Aes myAes = Aes.Create())
+            {
+                return DecryptStringFromBytes_Aes(Convert.FromBase64String(encryption), key, Convert.FromBase64String(Iv));
+            }
+        }
+
+        public static string KeyMask()
+        {
+            string pwd = "";
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd = pwd.Substring(0, pwd.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else if (i.KeyChar != '\u0000') // KeyChar == '\u0000' if the key pressed does not correspond to a printable character, e.g. F1, Pause-Break, etc
+                {
+                    pwd += i.KeyChar;
+                    Console.Write("*");
+                }
+            }
+            return pwd;
+        }
+
+        // THESE ARE THE FUNCTIONS FROM MICROSOFT DOCUMENTATION
+        // https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-9.0#code-try-2
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
---
+++ Pass Manager Prototype/Pass Manager Prototype/Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Pass_Manager;
+
+namespace Pass_Manger
+{
+    /*
+     * The prototype will use functions as different "states"
+    */ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (RetrieveMasterPass() == "") { CreateMasterPass(); }
+            if (Login()) { Landing(); }
+            else
+            {
+                Console.WriteLine("That is incorrect. Now Exiting");
+                Environment.Exit(0);
+            }
+        }
+        static void CreateMasterPass()
+        {
+            string password = "";
+            string encryptedPassword = "";
+            string salt = Convert.ToBase64String(Encryption.GenerateSalt());
+            Console.WriteLine("Please enter your desired master password");
+            password = Encryption.KeyMask();
+
+            encryptedPassword = Encryption.EncryptPassword_PBKDF2(password, Convert.FromBase64String(salt));
+
+            InsertMasterPassHash(encryptedPassword, salt);
+            Console.WriteLine("\nPassword entered thank you");
+        }
+        static bool Login()
+        {
+            // Opening sequance
+            Console.WriteLine("Welcome Dakota\nPlease enter your password");
+            string input = "";
+            input = Encryption.KeyMask();
+            string encryptedInput = "";
+            string masterPass = "";
+
+            // Check for match
+            masterPass = RetrieveMasterPass();
+            encryptedInput = Encryption.EncryptPassword_PBKDF2(input, Convert.FromBase64String(GetMasterSalt()));
+
+            if(masterPass == encryptedInput) { return true; }
+            return false;
+        }
+        static void Landing()
+        {
+            Console.Clear();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("[1] Get Password\n[2] Add Service\n[3] Exit\n");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": GetPassword(); break;
+                case "2": AddService(); break;
+                case "3": Environment.Exit(0); break;
+            }
+        }
+        static void GetPassword()
+        {
+            /*
+             * Get the password, salt, and Iv from the DB with provided service name
+             * Generate encryption key
+             * use information for decryption
+             * remove the salt from the password
+             * display the password
+             * Allow the user to enter "3" to return
+            */
+            string service = "";
+            string cipherText = "";
+            string salt = "";
+            string iv = "";
+
+            Console.Clear();
+            // Get an email and master password
+            Console.WriteLine("Please enter an email.\n");
+            string email = Console.ReadLine();
+            Console.WriteLine("\nPlease Enter the master password.\n");
+            string masterPassword = "";
+            masterPassword = Encryption.KeyMask();
+
+            Console.Clear();
+            Console.WriteLine("Please enter 3 if you are in the wrong menu. \nWhat service would you like the passwor for?\n");
+            service = Console.ReadLine();
+            if (service == "3") { Landing(); }
+
+            RetrieveService(service, ref cipherText, ref salt, ref iv);
+
+            string password = Encryption.DecryptPassword_AES(cipherText, Encryption.GenerateKey(email, masterPassword, Convert.FromBase64String(salt)), iv);
+
+            // Remove the salt from the password by knowing all passwords are generated with 15 characters before the salt is added
+            password = password.Remove(14);
+            Console.Clear();
+            Console.WriteLine($"Service: {service}\nPassword: {password}");
+
+            // Exit
+            Console.WriteLine("\n\nIf you are finished enter 3\n");
+            if (Console.ReadLine() == "3") { Landing(); }
+        }
+        static void AddService()
+        {
+            string serviceName = "";
+            string password = "";
+            string salt = "";
+            string IV = "";
+
+            Console.Clear();
+            Console.WriteLine("You have chosen to add a service. If this is wrong please enter 3.\nEnter name for the service:\n");
+            serviceName = Console.ReadLine();
+            serviceName = serviceName.ToLower();
+
+            if (serviceName == "3") { Landing(); }
+
+            /*
+             * Generate a password
+             * Display that password
+             * Generate Encryption key
+             * Add salt
+             * Encrypt using AES
+             * Save encrypted password to DB along with salt and IV
+             * Save service name
+            */
+
+            // Get the user to re-enter their master password and an email
+            Console.WriteLine("\nNow enter an email\n");
+            string email = Console.ReadLine();
+            Console.WriteLine("\nPlease enter the master password.\n");
+            string masterPass = "";
+            masterPass = Encryption.KeyMask();
+
+            password = Encryption.GenerateRandomPassword();
+            Console.WriteLine($"Password: {password}");
+            
+            salt = Convert.ToBase64String(Encryption.GenerateSalt());
+            IV = Convert.ToBase64String(Encryption.GenerateIv());
+
+            string saltedPassword = password + salt;
+
+            string encryptedPassword = Encryption.EncryptPassword_AES(saltedPassword, Encryption.GenerateKey(email, masterPass, Convert.FromBase64String(salt)), IV);
+
+            InsertService(serviceName, encryptedPassword, salt, IV);
+
+            // Exit this menu
+            Console.WriteLine("\nPlease enter 3 when ready to return.\n");
+            if(Console.ReadLine() == "3") { Landing(); }
+        }
+
+        // DB Access
+        static void InsertMasterPassHash(string password, string salt)
+        {
+            MySqlConnection cnn;
+            string connectionString = Environment.GetEnvironmentVariable("Connection String");
+            cnn = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand cmd = cnn.CreateCommand();
+                cmd.Connection = cnn;
+                cmd.CommandText = $"INSERT INTO userpass (Password, Salt) VALUES('{password}', '{salt}');";
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+        static string RetrieveMasterPass()
+        {
+            string password = "";
+            MySqlConnection cnn;
+            string connectionString = Environment.GetEnvironmentVariable("Connection String");
+            cnn = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand mySqlCommand = cnn.CreateCommand();
+                mySqlCommand.Connection = cnn;
+                mySqlCommand.CommandText = "SELECT Password FROM userpass;";
+
+                cnn.Open();
+                using var myReader = mySqlCommand.ExecuteReader();
+                myReader.Read();
+                password = myReader.GetString("Password");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            return password;
+        }
+        static string GetMasterSalt()
+        {
+            string salt = "";
+            MySqlConnection cnn;
+            string connectionString = Environment.GetEnvironmentVariable("Connection String");
+            cnn = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand cmd = cnn.CreateCommand();
+                cmd.Connection = cnn;
+                cmd.CommandText = $"SELECT Salt FROM userpass";
+
+                cnn.Open();
+                var myReader = cmd.ExecuteReader();
+                myReader.Read();
+                salt = myReader.GetString("Salt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            return salt;
+        }
+        static void InsertService(string service, string encryptedPass, string salt, string Iv)
+        {
+            MySqlConnection cnn;
+            string ConnectionString = Environment.GetEnvironmentVariable("Connection String");
+            cnn = new MySqlConnection(ConnectionString);
+            try
+            {
+                MySqlCommand cmd = cnn.CreateCommand();
+                cmd.Connection = cnn;
+                cmd.CommandText = $"INSERT INTO services (Service, Password, Salt, IV) VALUES('{service}', '{encryptedPass}', '{salt}', '{Iv}')";
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+        static void RetrieveService(string service,ref string cipherString, ref string salt, ref string iv)
+        {
+            service = service.ToLower();
+            MySqlConnection cnn;
+            string connectionString = Environment.GetEnvironmentVariable("Connection String");
+            cnn = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand cmd = cnn.CreateCommand();
+                cmd.Connection = cnn;
+                cmd.CommandText = $"SELECT * FROM services WHERE Service='{service}'";
+
+                cnn.Open();
+                var myReader = cmd.ExecuteReader();
+                myReader.Read();
+                cipherString = myReader.GetString("Password");
+                salt = myReader.GetString("Salt");
+                iv = myReader.GetString("IV");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+    }
+}
